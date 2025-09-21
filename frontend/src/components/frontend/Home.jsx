f440ad97ad02d172a434af205e9ff78a0c39e7c7
NDT28092005 <ndaitung28092005@gmail.com>
2025-09-21 16:34:36 +0700

    Commit ban đầu: seting prj

---
+++ frontend/src/components/frontend/Home.jsx
@@ -0,0 +1,76 @@
+import React from 'react'
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Dropdown from 'react-bootstrap/Dropdown';
+import { useNavigate } from "react-router-dom";
+import { FaSearch } from "react-icons/fa";
+const Home = () => {
+    const navigate = useNavigate();
+    return (
+        <header className="site-header">
+            <div className="container site-header__container">
+                <Navbar expand="lg" className="site-header__navbar">
+                    {/* Logo */}
+                    <Navbar.Brand href="/" className="site-header__brand">
+                        Cinestar
+                    </Navbar.Brand>
+                    {/* Action buttons */}
+                    <Nav className="site-header__actions">
+                        <Button
+                            className="site-header__btn"
+                            onClick={() => navigate("/movie")}
+                        >
+                            Đặt vé ngay
+                        </Button>
+                        <Button
+                            className="site-header__btn"
+                            onClick={() => navigate("/popcorn-drink")}
+                        >
+                            Đặt bắp nước
+                        </Button>
+                    </Nav>
+                    {/* Right side: search, login, language */}
+                    <Navbar.Collapse id="basic-navbar-nav" className="site-header__right">
+                        <div className="site-header__utilities ms-auto">
+                            {/* Search */}
+                            <Form inline className="site-header__search">
+                                <div className="input-group">
+                                    <Form.Control
+                                        type="text"
+                                        placeholder="Tìm phim"
+                                        className="site-header__search-input"
+                                    />
+                                    <button className="btn site-header__search-btn" type="button">
+                                        <FaSearch />
+                                    </button>
+                                </div>
+                            </Form>
+                            {/* Login */}
+                            <Nav>
+                                <Nav.Link href="/login" className="site-header__login">
+                                    Đăng nhập
+                                </Nav.Link>
+                            </Nav>
+                            {/* Language dropdown */}
+                            <Dropdown className="site-header__lang">
+                                <Dropdown.Toggle variant="success" size="sm">
+                                    VN
+                                </Dropdown.Toggle>
+                                <Dropdown.Menu>
+                                    <Dropdown.Item href="#/action-1">ENG</Dropdown.Item>
+                                </Dropdown.Menu>
+                            </Dropdown>
+                        </div>
+                    </Navbar.Collapse>
+                </Navbar>
+            </div>
+        </header>
+
+    )
+}
+
+export default Home
